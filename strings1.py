f47271df64eb46a1ec43aa1e013d8b95dc9ef217
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-11 20:11:31 +0530

    Add files via upload

---
+++ strings1.py
@@ -0,0 +1,11 @@
+'''
+DATE=11TH NOV 2022
+DAY= FRIDAY
+TOPIC= STRINGS 1
+AUTHOR= POOJA
+'''
+name="cricket"
+print(name.upper()) #UPPER
+print(name.isupper()) #false
+print(name.lower()) #cricket
+print(name.islower()) #true
