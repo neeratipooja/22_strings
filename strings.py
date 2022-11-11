60ebf6ba47d30c770ec07dbcba2938c9bd701f13
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-11 20:01:42 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,15 @@
+'''
+DATE=11TH NOV 2022
+DAY= FRIDAY
+TOPIC= STRINGS
+AUTHOR= POOJA
+'''
+n="hEllo PyThon"
+print(n.capitalize()) #Hello python
+name="hEllopyThon"
+print(name.capitalize()) #Hellopython
+name="hEllo pyThon"
+print(name.casefold()) #hello python
+print(name.center(50)) #                       hEllo pyThon
+print(name.center(50,'$')) #$$$$$$$$$$$$$$$$$$$hEllo pyThon$$$$$$$$$$$$$$$$$$$
+print(name.center(70,'%')) #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%hEllo pyThon%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
